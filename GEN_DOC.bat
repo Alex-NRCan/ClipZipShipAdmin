56c63cbec2826ec09b6e529039101e64fd1c516b
Alex-NRCan <alexandre.roy@nrcan-rncan.gc.ca>
2022-06-16 17:14:10 -0400

    Cleaned

--- GEN_DOC.bat
+++ GEN_DOC.bat
@@ -2,17 +2,17 @@ echo pip install Sphinx
 echo pip install sphinxcontrib-napoleon
 echo pip install sphinx-rtd-theme
 
-cd C:\!Quality\source\Quality\Python\docs
+cd C:\Users\alexandre.roy\pygeoapi\pygeoapi-admin\docs
 
-call sphinx-apidoc -o ../docs/source_api ..\core
-call sphinx-apidoc -o ../docs/source_api ..\api
+call sphinx-apidoc -o ../docs/api ..\core
+call sphinx-apidoc -o ../docs/api ..\api
 call make_api clean
 call make_api html
 
-call sphinx-apidoc -o ../docs/source_web ..\core
-call sphinx-apidoc -o ../docs/source_web ..\web
+call sphinx-apidoc -o ../docs/web ..\core
+call sphinx-apidoc -o ../docs/web ..\web
 call make_web clean
 call make_web html
 
-cd C:\!Quality\source\Quality\Python
+cd C:\Users\alexandre.roy\pygeoapi\pygeoapi-admin
 
